6a1faadfc3fbd7b80863ad758886565f37525706
Barney Hanlon <barney@shrikeh.net>
2024-11-12 11:37:26 +0000

    Prep for feature-based testing

--- tests/app/public/index.php
+++ tests/app/public/index.php
@@ -1,11 +1,13 @@
 <?php
-
+// phpcs:ignoreFile
 declare(strict_types=1);
 
 use App\Kernel;
 
-require_once dirname(__DIR__, 3) . '/vendor/autoload_runtime.php';
+defined('PROJECT_ROOT_DIR') || define('PROJECT_ROOT_DIR', 3);
 
-return function (array $context) {
+require_once dirname(__DIR__, PROJECT_ROOT_DIR) . '/vendor/autoload_runtime.php';
+
+return function (array $context): Kernel {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
